aff137dc3c37f60cd3289a15b44f76525db1484c
MichalJurek <108137462+MichalJurek@users.noreply.github.com>
2023-12-03 10:17:48 +0000

    Deploying to gh-pages from @ MichalJurek/goit-js-hw-09@e7661d76af091608dc13a2ddc7294a0ddf11da9b 🚀

---
+++ 01-color-switcher.3e650dd0.js
@@ -0,0 +1,2 @@
+!function(){let t=document.querySelector("[data-start]");document.querySelector("[data-stop]"),t.addEventListener("click",()=>{setInterval(()=>{document.body.style.backgroundCOlor=`#${Math.floor(16777215*Math.random()).toString(16)}`},1e3),t.disabled=!0})}();
+//# sourceMappingURL=01-color-switcher.3e650dd0.js.map
---
+++ 01-color-switcher.8b8d6f62.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");document.querySelector("[data-stop]"),t.addEventListener("click",()=>{setInterval(()=>{document.body.style.backgroundCOlor=`#${Math.floor(16777215*Math.random()).toString(16)}`},1e3),t.disabled=!0});
+//# sourceMappingURL=01-color-switcher.8b8d6f62.js.map
