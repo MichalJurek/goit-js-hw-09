8963b699336d5e84f0a3581fc36ab668d7873d90
Michał <jurekmichal@o2.pl>
2023-12-03 10:52:18 +0100

    Update 01-color-switcher.js
    first set

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let colorChangeIntervalId = null;
+
+const startCalorChange = () => {
+  colorChangeIntervalId = setInterval(() => {
+    document.body.style.backgroundCOlor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.disabled = true;
+};
+
+const stopColorChange = () => {
+  clearInterval(colorChangeIntervalId);
+  startBtn.disabled = false;
+};
+
+startBtn.addEventListener('click', startCalorChange);
