0b66b737109a5c1326dfe0669a341c6f5fb4e0c5
Michał <jurekmichal@o2.pl>
2023-12-03 12:21:58 +0100

    step 1
    pierwszy wklad

---
+++ src/js/02-timer.js
@@ -0,0 +1,47 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+
+let countdownIntervalId = null;
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { day, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const endDate = new Date(datetimePicker.value).getTime();
+  const remainingTime = endDate - now;
+
+  if (remainingTime < 0) {
+    clearInterval(countdownIntervalId);
+    alert('Please choose a date in the future.');
+    startButton.disabled = true;
+    return;
+  }
+
+  const { days, hours, minutes, seconds } = convertMs(remainingTime);
+
+  daysElement.textContent = addLeadingZero(days);
+  hoursElement.textContent = addLeadingZero(hours);
+  minutesElement.textContent = addLeadingZero(minutes);
+  secondsElement.textContent = addLeadingZero(seconds);
+}
