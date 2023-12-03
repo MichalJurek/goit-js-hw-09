0900640e7e9f478e329fa10a6676937937b618ac
MichalJurek <108137462+MichalJurek@users.noreply.github.com>
2023-12-03 10:24:24 +0000

    Deploying to gh-pages from @ MichalJurek/goit-js-hw-09@3dd69ab85fb7d941cba2b6adb796a8abab823e55 🚀

---
+++ 01-color-switcher.bb85c774.js
@@ -0,0 +1,2 @@
+!function(){let e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=null;e.addEventListener("click",()=>{a=setInterval(()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`},1e3),e.disabled=!0}),t.addEventListener("click",()=>{clearInterval(a),e.disabled=!1})}();
+//# sourceMappingURL=01-color-switcher.bb85c774.js.map
